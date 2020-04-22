c564244a9b6557f52e7e771dfeefe81512ff56b8
miguel.gonzalezg01@estudiante.uam.es <miguel.gonzalezg01@estudiante.uam.es>
2020-04-23 01:02:54 +0200

    Modulo Discovery funcionando. Modulo de control redactado pero sin probar.

---
+++ control.py
@@ -0,0 +1,507 @@
+'''
+    control.py
+    Modulo que se encarga de emitir las conexiones de control.
+    Incluye la funcion de escucha de conexiones de control.
+    @author Miguel Gonzalez, Alejandro Bravo.
+    @version 1.0
+    @date 22-04-2020
+'''
+
+'''
+    DESCRIPCION GENERAL DEL MODULO
+    El modulo hace uso de semaforos en sus funciones. Por ello es importante hacer las cosas en orden.
+
+    1. Preparar hilos de gestion de conexiones y de gestion de comandos con las funciones de bucle provistas.
+    2. Si se desea llamar, se usa connect_to(username) exclusivamente (hay otras funciones auxiliares pero por si solas no ajustan los semaforos)
+    3. Si se desea poner en espera o quitar, se usa set_hold()
+    4. Si se desea conocer si esta en llamada, y los parametros relevantes se usa call_status()
+    5. Los bucles gestionan las conexiones y comandos entrantes por si solos, de manera sincronizada.
+    6. Si se desea terminar la conexion, hay que hacer call_end() y control_disconnect().
+    7. Para detener los bucles se usan las funciones de stop.
+'''
+
+
+import socket
+from discovery import query_user
+import threading
+
+# Parametros
+socket_timeout = 3 #Timeout para receive en casos criticos.
+call_timeout = 15 #Timeout para responder a la llamada
+
+# ESTADO ACTUAL
+
+control_socket = None #Socket de control.
+connected_to = None #Nombre del usuario al que esta conectado.
+on_call_with = [None,None] #IP y puerto (como cadena) para transferencia de video. Si alguno es None, no esta en llamada.
+on_hold = False #Indica si se ha puesto la llamada en espera
+call_held = False #Indica si la otra parte ha puesto la llamada en espera.
+listen_end = False #Indica al hilo que escucha que debe parar.
+incoming_end = False #Indica al hilo que responde comandos entrantes que debe parar.
+connection_barrier = threading.Semaphore(0) #Barrera que impide el paso al hilo que responde comandos si no hay conexiones activas.
+global_lock = threading.Lock() #Cerrojo para las variables globales.
+
+
+# INFORMACION 
+def get_username():
+    '''
+        Nombre: get_username
+        Descripcion: Obtiene el nombre de usuario propio.
+        Argumentos: 
+        Retorno:
+            Nombre de usuario
+    '''
+    return "MiguelG2" #TODO leer de algun lado
+
+def get_video_port():
+    '''
+        Nombre: get_video_port
+        Descripcion: Obtiene el puerto en el que se recibe el video.
+        Argumentos: 
+        Retorno:
+            Puerto en el que se recibe el video.
+    '''
+    return "12345" #TODO leer de algun lado
+
+
+# CONTROL SALIENTE
+
+def connect_to(username,dstport):
+    '''
+        Nombre: connect_to
+        Descripcion: Inicializa la conexion de control con un usuario y lo llama.
+        Argumentos: username: Nombre de usuario.
+        Retorno:
+            0 si todo ha ido correctamente, -1 en caso de error.
+    '''
+    global connected_to, on_hold, on_call_with, call_held,global_lock
+
+    with global_lock:
+        connected_to_read = connected_to
+
+    if connected_to_read != None:
+        print("Error conectandose al usuario indicado. Ya esta conectado al usuario: " + connected_to_read)
+        return -1
+
+    #Obtenemos la IP y el puerto.
+    ret = query_user(username)
+    if ret == None:
+        print("Error conectandose al usuario indicado. El servidor reporta que no existe.")
+        return -1
+    if connect_to_addr(ret[0],ret[1]) == -1:
+        return -1
+    
+    #Ajuste de parametros
+    with global_lock:
+        connected_to = username
+        on_hold = False #Reinicia la espera de la llamada.
+        call_held = False
+        on_call_with = [ret[0], None] #Vamos ajustando la IP de video
+    return call(int(get_video_port())) #Se efectua la llamada
+    
+def connect_to_addr(ip, port):
+    '''
+        Nombre: connect_to_addr
+        Descripcion: Inicializa la conexion de control con un usuario.
+        Argumentos: ip: Ip a la que conectarse.
+                    port: puerto al que conectarse.
+        Retorno:
+            0 si todo ha ido correctamente, -1 en caso de error.
+    '''
+    global control_socket, connected_to, on_hold,global_lock
+
+    with global_lock:
+        #Zona protegida porque tocamos el socket global. 
+        #Ademas esto se ejecuta puntualmente (cuando el usuario llama), asi que no 
+        #interfiere en exceso con los hilos en bucle.
+
+        if(control_socket != None):
+            print("Ya hay una conexion de control establecida. Debe cerrarse primero.")
+            return -1
+
+        control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if control_socket == None:
+            return -1
+
+        print("Conectandose al usuario con ip " + ip + " y puerto " + port)
+        try:
+            control_socket.connect((ip,int(port)))
+        except TimeoutError:
+            print ("No ha sido posible conectarse al usuario. El usuario no ha aceptado la conexion en el tiempo establecido.")
+            control_socket.close()
+            control_socket = None
+            return -1
+
+    return 0
+
+def control_disconnect():
+    '''
+        Nombre: control_disconnect
+        Descripcion: Finaliza la conexion de control saliente abierta previamente.
+        Argumentos: 
+        Retorno:
+            0 si todo ha ido correctamente, -1 en caso de error.
+    '''
+    global control_socket, connected_to, on_hold, on_call_with, connection_barrier, global_lock
+
+    with global_lock:
+
+    #Zona protegida porque altera el estado.
+
+        if control_socket == None:
+            print ("Se ha intentado enviar un comando por la conexion de control sin estar conectado a el.")
+            return -1
+        control_socket.close()
+
+        #Reinicio de variables
+        control_socket = None
+        connected_to = None
+        on_hold = False
+        on_call_with = [None, None]
+        connection_barrier.acquire() #Bajamos el semaforo de conexion
+
+    return 0
+
+def call(dstport):
+    '''
+        Nombre: call
+        Descripcion: Llama a un usuario por la conexion de control ya abierta.
+        Argumentos: dstport: Puerto por el que se van a aceptar las conexiones, como entero.
+        Retorno:
+            En caso de que se haya aceptado, devuelve el puerto donde ha de enviarse el video, como entero.
+            En caso de error devuelve -1.
+            En caso de que el usuario destino este en llamada, devuelve -2.
+            En caso de que el usuario destino rechace la llamada, devuelve -3.
+            Siempre imprime por pantalla el resultado.
+    '''
+    global control_socket, connected_to, on_call_with, global_lock, connection_barrier
+
+    with global_lock:
+        #Zona protegida por operar con el socket
+        if control_socket == None or connected_to == None:
+            print ("Error llamando: no se esta conectado a ningun usuario.")
+            return None
+        mensaje = "CALLING " + get_username() + " " + str(dstport)
+        control_socket.send(mensaje.encode())
+        control_socket.settimeout(call_timeout)
+        try:
+            result = control_socket.recv(1024)
+        except:
+            print("Error iniciando llamada: el otro lado no ha respondido. Cerrando conexion...")
+            control_disconnect()
+            return -1
+        control_socket.settimeout(None)
+
+    if not result:
+        #El otro ha cerrado la conexion. El hilo de procesado de comandos cerrara nuestro lado. Salimos
+        print("Error iniciando llamada: el otro extremo ha cerrado la conexion.")
+        return -1
+
+    #Obtenemos las palabras de la respuesta
+    words = result.decode().split()
+    if len(words) == 0:
+        #No hay respuesta suficiente
+        print ("Error llamando usuario. El destinatario no respondio.")
+        return None
+    elif words[0] == "CALL_ACCEPTED":
+        #Acepta llamada
+        if len(words) < 3:
+            #Faltan datos
+            print("Error en la respuesta del destinatario. No ha devuelto nick o puerto destino.")
+            return None
+        #Hay timestamp
+        print("Llamada aceptada por destinatario. Desea puerto: " + words[2])
+        with global_lock:
+            on_call_with[1] = words[2] #Ajustamos el puerto de llamada.
+
+        connection_barrier.release() #Subimos el semaforo de conexion
+
+        return int(words[2])
+    elif words[0] == "CALL_DENIED":
+        #Rechaza llamada
+        print("Destinatario rechaza llamada. Desconectando...")
+        control_disconnect() #Desconectamos
+        return -3
+    elif words[0] == "CALL_BUSY":
+        #Rechaza llamada
+        print("Destinatario esta en llamada. Desconectando...")
+        control_disconnect() #Desconectamos
+        return -2
+    #Respuesta desconocida
+    print("Error desconocido llamando.")
+    return None
+
+def set_on_hold(held):
+    '''
+        Nombre: set_on_hold
+        Descripcion: Pone en espera la llamada actual.
+        Argumentos: held: true para poner en espera, false para reanudar.
+        Retorno:
+            0 si todo es correcto, -1 en caso de error.
+    '''
+    global control_socket, connected_to, on_hold, on_call_with, global_lock
+
+    with global_lock:
+        #Zona protegida porque opera con el estado y los sockets
+
+        #Si no hay conexiones, error.
+        if control_socket == None or connected_to == None:
+            print ("Error poniendo en espera: no esta conectado con ningun usuario.")
+            return None
+
+        if on_call_with[0] == None or on_call_with[1] == None:
+            print ("Error poniendo en espera: no esta en llamada.")
+            return None
+
+        if held:
+            #Si se quiere pausar. Comprobamos que no este pausada ya.
+            if on_hold:
+                print ("La llamada ya está en espera por parte de este extremo.")
+                return -1
+            mensaje = "CALL_HOLD " + get_username()
+            control_socket.send(mensaje.encode())
+            print("Llamada puesta en espera.")
+            on_hold = True
+        else:
+            #Si se quiere reanudar. Comprobamos que no este reanudada ya.
+            if not on_hold:
+                print("La llamada ya está reanudada desde este extremo.")
+                return -1
+            mensaje = "CALL_RESUME " + get_username()
+            control_socket.send(mensaje.encode())
+            print("Espera retirada de la llamada.")
+            on_hold = False
+    return 0
+
+def end_call():
+    '''
+        Nombre: end_call
+        Descripcion: Finaliza la llamada actual. No termina la conexion (para ello, ver control_disconnect)
+        Retorno:
+            0 si todo es correcto, -1 en caso de error.
+    '''
+    global control_socket, connected_to, on_hold, on_call_with, global_lock
+
+    with global_lock:
+        #Zona protegida porque opera con el estado y los sockets.
+
+        #Si no hay conexiones, error.
+        if control_socket == None or connected_to == None:
+            print ("Error finalizando llamada: no esta conectado con ningun usuario.")
+            return None
+
+        if on_call_with[0] == None or on_call_with[1] == None:
+            print ("Error finalizando llamada: no esta en llamada.")
+            return None
+
+        mensaje = "CALL_END " + get_username()
+        control_socket.send(mensaje.encode())
+
+        on_call_with[1] = None
+    return 0
+
+def call_status():
+    '''
+        Nombre: call_status
+        Descripcion: Permite al cliente saber si debe transferir/recibir video y a donde.
+        Retorno:
+            + Lista [ip,puerto] si el usuario esta en llamada. El video debe mandarse a esa ip+puerto.
+            + Si la llamada esta en espera por nuestra parte, devuelve "HOLD1" en la ip
+            + Si la llamada esta en espera por parte opuesta, devuelve "HOLD2" en la ip
+            + Si no esta en llamada, devuelve None en la ip.
+    '''
+    global on_hold, on_call_with, call_held, global_lock
+
+    with global_lock:
+        if on_call_with[0] == None or on_call_with[1] == None:
+            return [None,None]
+        elif on_hold:
+            return ["HOLD1", None]
+        elif call_held:
+            return ["HOLD2", None]
+        else:
+            return on_call_with
+
+
+#CONTROL ENTRANTE
+
+#Esta rutina se ejecutara en un hilo aparte de las demas, para controlar los comandos que entran.
+def control_incoming_loop():
+    '''
+        Nombre: control_incoming_loop
+        Descripcion: Gestiona el control entrante.
+        Argumentos:
+        Retorno:
+            Imprime salida por pantalla. -1 en caso de error, 0 en caso correcto.
+    '''
+    global connection_barrier, incoming_end, global_lock, call_held
+
+    with global_lock:
+        incoming_end_read = incoming_end
+
+    print("Hilo de procesado de comandos operativo.")
+    while not incoming_end_read:
+        connection_barrier.acquire() #Esperamos a que haya una conexion activa.
+        print("Conexion detectada. Hilo de procesado de mensajes intentando recibir...")
+        
+        if(control_socket != None):
+            try:
+                msg = control_socket.recv(1024) #Recibimos mensaje
+            except:
+                #Esto sucede ante una desconexion desde nuestro lado. Simplemente seguimos.
+                connection_barrier.release()
+                continue
+            if not msg: #La unica posibilidad de fallo es que el otro cierre la conexion.
+                connection_barrier.release()
+                control_disconnect() #Cerramos la nuestra
+                print("Cerrando conexion de control actual ante el cierre por la parte contraria.")
+                continue
+            
+            msg.decode()
+
+            print("Hilo de procesado de mensajes recibe: " + msg)
+        else:
+            connection_barrier.release()
+            continue
+
+        words = msg.split()
+        will_end = False
+
+        if(len(words) < 1):
+            #El mensaje esta vacio
+            connection_barrier.release()
+            continue
+
+        if(words[0] == "CALL_HOLD" and len(words) < 2):
+            print("El usuario " + words[1] + " pone la llamada en espera.")
+            #Poner en espera
+            with global_lock:
+                call_held = True
+        elif(words[0] == "CALL_RESUME" and len(words) < 2):
+            print("El usuario " + words[1] + " retira su espera.")
+            #Quitar espera
+            with global_lock:
+                call_held = False
+        elif(words[0] == "CALL_END"):
+            will_end = True
+
+        connection_barrier.release()
+
+        #Cerramos la conexion. Debe hacerse aqui despues para evitar
+        #problemas con la barrera.
+        if will_end:
+            control_disconnect()
+            will_end = False
+
+        #Recuperamos el estado del bucle
+        with global_lock:
+            incoming_end_read = incoming_end
+
+    print("Hilo de procesado de comandos saliendo...")
+
+def control_incoming_stop():
+    '''
+        Nombre: control_incoming_stop
+        Descripcion: Detiene el procesamiento de mensajes de control entrantes
+    '''
+    global incoming_end, global_lock
+    with global_lock:
+        incoming_end = True
+
+
+#ESCUCHA DE CONEXIONES NUEVAS
+
+#Esta rutina se ejecutara en un hilo aparte de las demas, para poder estar a la escucha. Por tanto usaremos Locks para proteger
+#las globales que modifique
+def control_listen_loop(port):
+    '''
+        Nombre: control_listen_loop
+        Descripcion: Establece un socket TCP a la escucha en el puerto pasado y gestiona las nuevas conexiones.
+        Argumentos:
+                port: puerto de escucha como cadena. Por ejemplo "1234"
+        Retorno:
+            Imprime salida por pantalla. -1 en caso de error, 0 en caso correcto.
+    '''
+    global listen_end,connected_to,control_socket,on_call_with, on_hold, call_held,connection_barrier, global_lock
+
+    socket_escucha = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if socket_escucha == None:
+        print("Error abriendo socket para escuchar conexiones de control.")
+        return -1
+    
+    #Asociar al puerto
+    socket_escucha.bind('', int(port))
+    socket_escucha.listen(5) #Escuchar
+
+    with global_lock:
+        listen_end_read = listen_end
+
+    while not listen_end_read:
+        #Bucle que se ejecutara continuamente.
+        print("Hilo de escucha de peticiones operativo.")
+
+        #Aceptar conexion
+        connection, addr= socket_escucha.accept()
+        print ("Aceptada conexion desde " + addr)
+
+        #Vemos a ver si esta intentando llamar.
+        connection.settimeout(socket_timeout) #Ponemos un timeout por si no responden
+        try:
+            msg = connection.recv(1024)
+        except socket.timeout:
+            print ("Conexion rechazada ante la falta de comandos.")
+            connection.close()
+            continue
+        connection.settimeout(None)
+
+        if not msg: #En caso de que nos cierren la conexion.
+            connection.close()
+            continue
+
+        msg.decode()
+        words = msg.split()
+
+        #Si no esta intentando llamar cerramos
+        if(len(words) < 3 or words[0] != "CALLING"):
+            print("Llamada denegada por peticion malformada.")
+            end_call = "CALL_DENIED " + get_username()
+            connection.send(end_call.encode())
+            connection.close()
+            continue
+
+        #Si ya estamos conectados estamos ocupados.
+        with global_lock:
+            connected_to_read = connected_to
+            control_socket_read = control_socket
+        if(connected_to_read != None or control_socket_read != None):
+            print("Ya hay una conexion activa. Respondiendo como llamada ocupada.")
+            connection.send(b'CALL_BUSY')
+            connection.close()
+            continue
+
+        #TODO Comprobar si el usuario ha rechazado la conexion
+
+        #Si no, esta pasa a ser la conexion actual
+        with global_lock:
+            print("Llamada aceptada. Cambiando conexion de control actual...")
+            control_socket = connection
+            connected_to = words[1]
+            on_call_with = [addr,words[2]]
+            on_hold = False
+            call_held = False
+            connection_barrier.release() #Levantamos la barrera de conexion
+            start_call = "CALL_ACCEPTED " + get_username() + " " + get_video_port()
+            connection.send(start_call)
+            listen_end_read = listen_end
+
+    print("Hilo de escucha de peticiones saliendo...")
+    return 0
+
+def control_listen_stop():
+    '''
+        Nombre: control_listen_stop
+        Descripcion: Detiene la escucha de nuevas conexiones
+    '''
+    global listen_end, global_lock
+    with global_lock:
+        listen_end = True
---
+++ discovery.py
@@ -0,0 +1,188 @@
+'''
+    discovery.py
+    Modulo que se encarga de la comunicacion con el servidor de descubrimiento.
+    @author Miguel Gonzalez, Alejandro Bravo.
+    @version 1.0
+    @date 22-04-2020
+'''
+
+import socket
+
+server_socket = None #Socket del servidor.
+server_ip = "tfg.eps.uam.es"
+server_port = 8000
+
+def server_init():
+    '''
+        Nombre: server_init
+        Descripcion: Inicializa la conexion con el servidor
+        Argumentos: 
+        Retorno:
+            0 si todo ha ido correctamente, -1 en caso de error.
+    '''
+    global server_socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if server_socket == None:
+        return -1
+
+    print("Conectandose al servidor de descubrimiento...")
+    try:
+        server_socket.connect((server_ip,server_port))
+    except TimeoutError:
+        print ("No ha sido posible conectarse al servidor de descubrimiento.")
+        server_socket.close()
+        server_socket = None
+        return -1
+
+    return 0
+
+def server_quit():
+    '''
+        Nombre: server_quit
+        Descripcion: Termina la conexion con el servidor, informando al server y limpiando recursos.
+        Argumentos: 
+        Retorno:
+            0 si todo ha ido correctamente, -1 en caso de error.
+    '''
+    global server_socket
+    if server_socket == None:
+        print ("Se ha intentado enviar un comando al servidor sin estar conectado a el.")
+        return -1
+    server_socket.send(b'QUIT')
+    result = server_socket.recv(1024)
+    if result.decode() != "BYE":
+        print ("Advertencia: El servidor no ha respondido al server_quit. Cerrando conexion...")
+    server_socket.close()
+    server_socket = None
+    return 0
+
+
+
+def register_user(nickname, password, protocols, ip, port):
+    '''
+        Nombre: register_user
+        Descripcion: Registra un usuario en el sistema.
+        Argumentos: nickname: Nombre de usuario a registrar.
+                    password: Clave del usuario
+                    protocols: Lista de protocolos que soporta
+                    ip: Direccion IP desde la que escucha
+                    port: Puerto desde el que escucha
+        Retorno:
+            Timestamp de registro o None en caso de error.
+    '''
+    global server_socket
+    if server_socket == None:
+        print ("Se ha intentado enviar un comando al servidor sin estar conectado a el.")
+        return None
+    cadena_protocolos = "#".join(protocols) #Creamos la cadena como especifica el protocolo
+    mensaje = "REGISTER " + nickname + " " + ip + " " + port + " " + password + " " + cadena_protocolos
+    server_socket.send(mensaje.encode())
+    result = server_socket.recv(1024)
+
+    #Obtenemos las palabras de la respuesta
+    words = result.decode().split()
+    if len(words) < 2:
+        #No hay respuesta suficiente
+        print ("Error registrando usuario. El servidor no respondio.")
+        return None
+    elif words[0] == "OK" and words[1] == "WELCOME":
+        #Respuesta correcta
+        if len(words) < 3:
+            #No hay timestamp
+            print("Usuario registrado correctamente. El servidor no ha proporcionado timestamp.")
+            return "0"
+        #Hay timestamp
+        print("Usuario registrado correctamente en timestamp: " + words[2])
+        return words[2]
+    elif words[0] == "NOK" and words[1] == "WRONG_PASS":
+        #Respuesta incorrecta
+        print("Error registrando usuario. Contraseña incorrecta.")
+        return None
+    #Respuesta desconocida
+    print("Error desconocido registrando usuario.")
+    return None
+
+def query_user(nickname):
+    '''
+        Nombre: query_user
+        Descripcion: Busca la informacion del usuario dado en el sistema.
+        Argumentos: nickname: Nombre del usuario.
+        Retorno:
+            Lista con IP, puerto y lista de protocolos soportados por el cliente, todo como cadenas. None en caso de error.
+    '''
+    global server_socket
+    if server_socket == None:
+        print ("Se ha intentado enviar un comando al servidor sin estar conectado a el.")
+        return None
+    mensaje = "QUERY " + nickname
+    server_socket.send(mensaje.encode())
+    result = server_socket.recv(1024)
+    words = result.decode().split()
+    if len(words) < 2:
+        #No hay respuesta suficiente
+        print ("Error buscando usuario. El servidor no respondio.")
+        return None
+    elif words[0] == "OK" and words[1] == "USER_FOUND":
+        #Respuesta correcta
+        if len(words) < 6:
+            #No hay datos en la respuesta
+            print("Error en QUERY: El servidor ha reportado que existe el usuario, pero no ha devuelto sus datos.")
+            return -1
+        #Hay datos en la respuesta
+        print("Usuario " + words[2] + " encontrado correctamente con IP: " + words[3] + ", puerto: " + words[4] + " y protocolos: " + words[5])
+        lista_protocolos = words[5].split("#")
+        return [words[3],words[4],lista_protocolos]
+    elif words[0] == "NOK" and words[1] == "USER_UNKNOWN":
+        #Respuesta incorrecta
+        print("Error buscando usuario: No existe usuario con ese nombre.")
+        return None
+    #Respuesta desconocida.
+    print("Error desconocido buscando usuario.")
+    return None
+
+def list_users():
+    '''
+        Nombre: list_users
+        Descripcion: Muestra un listado de todos los usuarios en el sistema.
+        Argumentos:
+        Retorno:
+            Lista con los datos de cada usuario. Cada dato es [nick ip port ts]. None en caso de error.
+    '''
+    global server_socket
+    if server_socket == None:
+        print ("Se ha intentado enviar un comando al servidor sin estar conectado a el.")
+        return None
+    mensaje = "LIST_USERS"
+    server_socket.send(mensaje.encode())
+    result = server_socket.recv(1024)
+    words = result.decode().split()
+    if len(words) < 2:
+        #No hay respuesta suficiente
+        print ("Error listando usuarios. El servidor no respondio.")
+        return None
+    elif words[0] == "OK" and words[1] == "USERS_LIST":
+        #Respuesta correcta
+        if len(words) < 3:
+            #No hay datos en la respuesta
+            print("Advertencia en LIST: El servidor no ha devuelto ningun usuario.")
+            return []
+        #Obtenemos lista de usuarios.
+        lista_usuarios = " ".join(words[3:]).split("#") #A partir de la palabra 3 estan los usuarios
+        lista_usuarios = lista_usuarios[:-1] #Eliminamos el espacio en blanco final.
+
+        #Los imprimimos
+        i = 1
+        print(str(len(lista_usuarios)) + " usuarios encontrados: ")
+        for usuario in lista_usuarios:
+            print(str(i) + ": " + usuario)
+            i += 1
+
+        return list(map(lambda x : x.split() , lista_usuarios))
+
+    elif words[0] == "NOK" and words[1] == "USER_UNKNOWN":
+        #Respuesta incorrecta
+        print("Error listando usuarios. Servidor da respuesta erronea.")
+        return None
+    #Respuesta desconocida.
+    print("Error desconocido listando usuarios.")
+    return None
