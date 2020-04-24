095ac86d0848aa0aa0797d8eb74bad0568bfecfa
alejandro.bravodela <alejandro.bravodela@estudiante.uam.es>
2020-04-24 02:50:24 +0200

    Inicio del modulo de video

--- practica3_client.py
+++ practica3_client.py
@@ -1,13 +1,14 @@
-# import the library
+# import the libraries
 from appJar import gui
 from PIL import Image, ImageTk
 import numpy as np
 import cv2
-from discovery import server_init, server_quit, register_user, query_user
 import threading
-from control import control_incoming_loop, control_listen_loop,control_listen_stop,control_incoming_stop, user_filename
 import socket
 import json
+from discovery import server_init, server_quit, register_user, query_user
+from control import control_incoming_loop, control_listen_loop,control_listen_stop,control_incoming_stop, user_filename, call_status
+from video import send_frame
 
 
 
@@ -18,7 +19,7 @@ class VideoClient(object):
 	nick = None
 	listen_control_port = "10000"
 	def __init__(self, window_size):
-		
+
 		# Creamos una variable que contenga el GUI principal
 		self.app = gui("Redes2 - P2P", window_size)
 		self.app.setGuiPadding(10,10)
@@ -35,7 +36,7 @@ class VideoClient(object):
 
 		# Añadir los botones
 		self.app.addButtons(["Conectar", "Colgar", "Salir"], self.buttonsCallback)
-		
+
 		# Barra de estado
 		# Debe actualizarse con información útil sobre la llamada (duración, FPS, etc...)
 		self.app.addStatusbar(fields=2)
@@ -66,7 +67,7 @@ class VideoClient(object):
 			self.app.stop()
 			return
 
-		# Entrada del nick del usuario a conectar    
+		# Entrada del nick del usuario a conectar
 		print("Comenzando registro de usuario.")
 		register_nick = self.app.textBox("Inicio", "Introduce tu nick de usuario.")
 		password = self.app.textBox("Inicio", "Introduce tu contrasena.")
@@ -87,7 +88,7 @@ class VideoClient(object):
 			if not ret:
 				#Error
 				self.app.errorBox("Error", "No se ha podido realizar el registro correctamente. Error del servidor de descubrimiento.")
-				self.app.stop() 
+				self.app.stop()
 				return
 
 			#Clave incorrecta
@@ -97,8 +98,8 @@ class VideoClient(object):
 
 		self.app.infoBox("Registro correcto", "El registro se ha realizado correctamente.")
 		self.listen_control_port = control_port
-		
-		#Hilos de recepcion	
+
+		#Hilos de recepcion
 		self.connection_loop = threading.Thread(target=control_listen_loop, args = (self.listen_control_port,))
 		self.command_loop = threading.Thread(target=control_incoming_loop)
 		self.connection_loop.start()
@@ -109,46 +110,65 @@ class VideoClient(object):
 			json.dump(user_data,file, indent=4)
 
 
+		#Iniciamos los sockets de video
+		socket_video_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		socket_video_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
 	# Función que captura el frame a mostrar en cada momento
 	def capturaVideo(self):
-		
+
 		# Capturamos un frame de la cámara o del vídeo
 		ret, frame = self.cap.read()
 		frame = cv2.resize(frame, (640,480))
-		cv2_im = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-		img_tk = ImageTk.PhotoImage(Image.fromarray(cv2_im))		    
 
-		# Lo mostramos en el GUI
+		# Código que envia el frame a la red
+		status = call_status()
+		if(status[0] != None and status[0] != "HOLD1" and status[0] != "HOLD2"):
+			send_frame(socket_video_send, status, frame, num, 50, "640x480", 40)
+			num += 1
+
+			#Esto hay que moverlo a un thread aparte
+			server_socket.bind((status[0],status[1]))
+			data, server = socket_video_rec.recvfrom(1024)
+			frame_rec = decompress(data)
+
+			frame_peque = cv2.resize(frame, (320,240)) # ajustar tamaño de la imagen pequeña
+			frame_compuesto = frame_rec
+			frame_compuesto[0:frame_peque.shape[0], 0:frame_peque.shape[1]] = frame_peque
+			frame = frame_compuesto
+
+		#Mostramos por la GUI ambas webcams o solo la nuestra si no hay ninguna llamada
+		cv2_im = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+		img_tk = ImageTk.PhotoImage(Image.fromarray(cv2_im))
 		self.app.setImageData("video", img_tk, fmt = 'PhotoImage')
 
-		# Aquí tendría que el código que envia el frame a la red
-		# ...
 
 	# Establece la resolución de la imagen capturada
-	def setImageResolution(self, resolution):		
+	def setImageResolution(self, resolution):
 		# Se establece la resolución de captura de la webcam
 		# Puede añadirse algún valor superior si la cámara lo permite
 		# pero no modificar estos
 		if resolution == "LOW":
-			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160) 
-			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120) 
+			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
 		elif resolution == "MEDIUM":
-			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) 
-			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) 
+			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 		elif resolution == "HIGH":
-			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) 
-			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) 
-				
+			self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+			self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 	# Función que gestiona los callbacks de los botones
 	def buttonsCallback(self, button):
 
-	    if button == "Salir":
-	    	# Salimos de la aplicación
-	        self.app.stop()
-	    elif button == "Conectar":
-	        # Entrada del nick del usuario a conectar    
-	        nick = self.app.textBox("Conexión", 
-	        	"Introduce el nick del usuario a buscar")        
+		if button == "Salir":
+			# Salimos de la aplicación
+			self.app.stop()
+		elif button == "Conectar":
+			# Entrada del nick del usuario a conectar
+			nick = self.app.textBox("Conexión",
+				"Introduce el nick del usuario a buscar")
 
 if __name__ == '__main__':
 
---
+++ video.py
@@ -0,0 +1,39 @@
+'''
+   video.py
+   Modulo encargado del apartado criptográfico de securebox
+   @author Alejandro Bravo, Miguel Gonzalez
+   @version 1.0
+   @date 23-04-2020
+'''
+
+import socket
+import time
+
+def send_frame(socket_video,status,frame,numOrden,quality,resolution,fps):
+    encimg = compress(frame,quality)
+    header = str(numOrden) + "#" + str(time.time()) + "#" + resolution + "#" + fps + "#"
+    message = bytearray(header + encimg)
+    lengthTot = len(message)
+    lengthSend = socket_video.sendto(message,status)
+    if(lengthSend != lengthTot):
+        return -1
+    return 0
+
+def compress(frame,quality):
+    # Compresión JPG al 50% de resolución (se puede variar)
+    encode_param = [cv2.IMWRITE_JPEG_QUALITY,quality]
+    result,encimg = cv2.imencode('.jpg',frame,encode_param)
+
+    if result == False: print('Error al codificar imagen')
+
+    return encimg
+
+def decompress(encimg):
+    # Descompresión de los datos, una vez recibidos
+    decimg = cv2.imdecode(np.frombuffer(encimg,np.uint8), 1)
+
+    # Conversión de formato para su uso en el GUI
+    #cv2_im = cv2.cvtColor(decimg,cv2.COLOR_BGR2RGB)
+    #img_tk = ImageTk.PhotoImage(Image.fromarray(cv2_im))
+
+    return img_tk
